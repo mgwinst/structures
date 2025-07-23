5efda40fb2898130e4a6bc2f8c1b64655416286a
mgwinst <matteospatola@hotmail.com>
2025-07-23 03:41:40 +0000

    heap -> stack managed array

--- array.h
+++ array.h
@@ -3,45 +3,41 @@
 #include <utility>
 
 template <typename T, std::size_t size>
-class array {
-public:
-    array() : ptr_{ new T[size] } {}
-
-    template <std::size_t N>
-    array(const T (&arr)[N]) : ptr_{ new T[size] } {
-        std::copy(arr, arr + N, ptr_);
+struct array {
+    T data_[size];
+    
+    constexpr T& at(std::size_t index) noexcept {
+        static_assert(index >= 0 && index < size, "array index out of bounds");
+        return data_[index];
     }
 
-    array(const array& arr) {
-        ptr_ = new T[size];
-        if (!ptr_) throw std::bad_alloc{};
-        for (std::size_t i = 0; i < size; i++) {
-            ptr_[i] = arr.ptr_[i];
-        }
+    constexpr T& operator[](std::size_t index) noexcept { return data_[index]; }
+    constexpr T& front() noexcept { return data_[0]; }
+    constexpr T& back() noexcept { return data_[size-1]; }
+    constexpr const T& operator[](std::size_t index) const noexcept { return data_[index]; }
+    constexpr const T& front() const noexcept { return data_[0]; }
+    constexpr const T& back() const noexcept { return data_[size-1]; }
+
+    constexpr T* data() noexcept { return data_; }
+    constexpr const T* data() const noexcept { return data_; }
+
+    constexpr T* begin() noexcept { return data_; }
+    constexpr T* end() noexcept { return data_ + size; }
+    constexpr const T* cbegin() const noexcept { return data_; }
+    constexpr const T* cend() const noexcept { return data_ + size; }
+
+    constexpr std::size_t size() const noexcept { return size; }
+    constexpr bool empty() const noexcept { return size == 0; }
+    
+
+    constexpr void fill(const T& value) { 
+        std::fill(data_, data_ + size, value);
     }
 
-    array& operator=(array arr) {
-        swap(*this, arr);
-        return *this;
+    friend constexpr void swap(const array& a, const array& b) {
+        std::swap_ranges(a.data_, a.data_ + size, b.data_);
     }
 
-    friend void swap(array& a, array& b) noexcept {
-        std::swap(a.ptr_, b.ptr_);
-    }
-
-    array(array&& arr) noexcept {
-        ptr_ = std::exchange(arr.ptr_, nullptr);
-    }
-
-    array& operator=(array&& arr) noexcept {
-        delete[] ptr_;
-        ptr_ = std::exchange(arr.ptr_, nullptr);
-        return *this;
-    }
-
-    ~array() { delete[] ptr_; }
-
-private:
-    T* ptr_{ nullptr };
+    ~array() = default;
 };
 
