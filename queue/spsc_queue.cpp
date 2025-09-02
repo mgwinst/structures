23458852d92b4c6fe34df914ed9c3f1dd50470a3
mgwinst <matteospatola@hotmail.com>
2025-09-02 22:44:38 +0000

    small change to queue

--- queue/spsc_queue.cpp
+++ queue/spsc_queue.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <algorithm>
 #include <print>
+#include <memory>
 
 #ifdef __cpp_lib_hardware_interference_size
     inline static constexpr std::size_t cache_line_size = std::hardware_destructive_interference_size;
@@ -21,18 +22,15 @@ namespace lib
             if (capacity <= 0 || ((capacity & (capacity - 1)) != 0)) {
                 throw std::invalid_argument("queue capacity must be power of 2");
             }
-            data_ = new std::byte[capacity];
-        }
-
-        ~spsc_queue()
-        {
-            delete[] data_;
+            capacity_ = capacity;
+            data_ = std::make_unique<std::byte[]>(capacity);
         }
 
         spsc_queue(const spsc_queue&) = delete;
         spsc_queue& operator=(const spsc_queue&) = delete;
         spsc_queue(spsc_queue&&) = delete;
         spsc_queue& operator=(spsc_queue&&) = delete;
+        ~spsc_queue() = default;
 
         auto write(std::span<std::byte> buffer) -> std::size_t
         {
@@ -50,10 +48,10 @@ namespace lib
             std::size_t bytes_to_end = capacity_ - index;
 
             if (bytes_to_write <= bytes_to_end) {
-                std::memcpy(data_ + index, buffer.data(), bytes_to_write);
+                std::memcpy(data_.get() + index, buffer.data(), bytes_to_write);
             } else {
-                std::memcpy(data_ + index, buffer.data(), bytes_to_end);
-                std::memcpy(data_, buffer.data() + bytes_to_end, bytes_to_write - bytes_to_end);
+                std::memcpy(data_.get() + index, buffer.data(), bytes_to_end);
+                std::memcpy(data_.get(), buffer.data() + bytes_to_end, bytes_to_write - bytes_to_end);
             }
 
             write_counter_.store(write_pos + bytes_to_write, std::memory_order_release);
@@ -76,10 +74,10 @@ namespace lib
             std::size_t bytes_to_end = capacity_ - index;
 
             if (bytes_to_read < bytes_to_end) {
-                std::memcpy(buffer.data(), data_ + index, bytes_to_read);
+                std::memcpy(buffer.data(), data_.get() + index, bytes_to_read);
             } else {
-                std::memcpy(buffer.data(), data_ + index, bytes_to_end);
-                std::memcpy(buffer.data() + bytes_to_end, data_, bytes_to_read - bytes_to_end);
+                std::memcpy(buffer.data(), data_.get() + index, bytes_to_end);
+                std::memcpy(buffer.data() + bytes_to_end, data_.get(), bytes_to_read - bytes_to_end);
             }
 
             read_counter_.store(read_pos + bytes_to_read, std::memory_order_release);
@@ -90,12 +88,12 @@ namespace lib
         private:
             alignas(cache_line_size) std::atomic<std::size_t> read_counter_{ 0 };
             alignas(cache_line_size) std::atomic<std::size_t> write_counter_{ 0 };
-            alignas(cache_line_size) std::byte* data_;
+            std::unique_ptr<std::byte[]> data_;
             std::size_t capacity_;
     };
 }
 
 int main()
 {
-
+    lib::spsc_queue queue{ 1024 };
 }
